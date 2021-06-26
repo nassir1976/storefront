6c58cd74c24e7c91216a4f05b6a10b9ec4a755b2
nassir <daniya_n@yahoo.com>
2021-06-26 13:21:53 -0700

    lab-38 strart

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers,applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import CartReducer from "./cart.js";
 import categoriesReducer from "./categories.js";
 import productsReducer from "./product";
-
+import thunk from './middleware/thunk.js';
+import loggerMiddleware from './middleware/logger.js';
 
 
 // combine reducer .... products & categories  are 
@@ -15,7 +16,7 @@ const reducers = combineReducers({
 })
 
 const store =()=>{
-  return createStore(reducers,composeWithDevTools());
+  return createStore(reducers,composeWithDevTools(applyMiddleware(loggerMiddleware, thunk)));
 }
 
 export default store;
